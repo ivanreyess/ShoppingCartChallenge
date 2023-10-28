fb296cf41d3d109ccf63e0b597b5bd42834d7806
ivanreyess <melvin_reyes@unicomer.com>
2023-10-28 17:17:11 -0600

    :white_check_mark: adding tests for order details service

--- orderDetailService/src/test/java/com/sv/orderdetailservice/domain/OrderDetailTest.java
+++ orderDetailService/src/test/java/com/sv/orderdetailservice/domain/OrderDetailTest.java
@@ -1,22 +1,54 @@
 package com.sv.orderdetailservice.domain;
 
+import com.sv.orderdetailservice.domain.dto.OrderDetailDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class OrderDetailTest {
 
-    //TODO test builders
-
+    OrderDetail orderDetail;
+    OrderDetailDTO orderDetailDTO;
 
     @BeforeEach
     void setUp() {
+        orderDetail = OrderDetail.builder()
+                .id(1L)
+                .orderId(1L)
+                .quantity(2)
+                .productId(1)
+                .price(2d)
+                .createdDate(1L)
+                .modifiedDate(1L)
+                .build();
+
+        orderDetailDTO = OrderDetailDTO.builder()
+                .orderId(1L)
+                .id(1L)
+                .productId(1)
+                .price(2d)
+                .quantity(2)
+                .createdDate(1L)
+                .modifiedDate(1L)
+                .build();
     }
 
     @Test
     void toEntity() {
+        OrderDetail orderDetail1 = OrderDetail.toEntity(orderDetailDTO);
+        assertEquals(1, orderDetail1.getOrderId());
+        assertEquals(1, orderDetail1.getProductId());
+        assertEquals(2, orderDetail1.getQuantity());
+        assertEquals(2, orderDetail1.getPrice());
     }
 
     @Test
     void toDto() {
+        OrderDetailDTO orderDetailDTO1 = OrderDetail.toDto(orderDetail);
+        assertEquals(1, orderDetailDTO1.orderId());
+        assertEquals(1, orderDetailDTO1.productId());
+        assertEquals(2, orderDetailDTO1.quantity());
+        assertEquals(2, orderDetailDTO1.price());
     }
 }
