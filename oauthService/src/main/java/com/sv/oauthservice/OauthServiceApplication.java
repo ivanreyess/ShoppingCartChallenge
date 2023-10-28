1440e6d69625791801a13711c454b3624300914c
ivanreyess <melvin_reyes@unicomer.com>
2023-10-28 09:54:50 -0600

    :sparkles: adding oauth service

---
+++ oauthService/src/main/java/com/sv/oauthservice/OauthServiceApplication.java
@@ -0,0 +1,15 @@
+package com.sv.oauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OauthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthServiceApplication.class, args);
+    }
+
+}
